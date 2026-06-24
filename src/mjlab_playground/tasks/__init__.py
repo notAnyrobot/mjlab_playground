952c9d2167cad4295f950a31816998ed94eb4d6f
notAnyrobot <liao.shiwen@qq.com>
2026-06-24 15:32:04 +0800

    refactor: localize getup task registrations

--- src/mjlab_playground/tasks/__init__.py
+++ src/mjlab_playground/tasks/__init__.py
@@ -1,5 +1,3 @@
 """Task collection for mjlab_playground."""
 
-from mjlab_playground.tasks.getup.config.astro import *  # noqa: F401, F403
-from mjlab_playground.tasks.getup.config.go1 import *  # noqa: F401, F403
-from mjlab_playground.tasks.getup.config.t1 import *  # noqa: F401, F403
+from mjlab_playground.tasks import getup as getup
--- src/mjlab_playground/tasks/getup/__init__.py
+++ src/mjlab_playground/tasks/getup/__init__.py
@@ -1,1 +1,5 @@
 """Getup (fall recovery) task for legged robots."""
+
+from mjlab_playground.tasks.getup.config.astro import *  # noqa: F401, F403
+from mjlab_playground.tasks.getup.config.go1 import *  # noqa: F401, F403
+from mjlab_playground.tasks.getup.config.t1 import *  # noqa: F401, F403
