bfceda90fed30a182f868ea1c61fd1f6870559bb
notAnyrobot <liao.shiwen@qq.com>
2026-06-26 22:47:30 +0800

    feat: export getup policies with metadata

--- src/mjlab_playground/tasks/getup/config/astro/__init__.py
+++ src/mjlab_playground/tasks/getup/config/astro/__init__.py
@@ -1,5 +1,6 @@
-from mjlab.rl import MjlabOnPolicyRunner
 from mjlab.tasks.registry import register_mjlab_task
+
+from mjlab_playground.tasks.getup.rl.runner import GetupOnPolicyRunner
 
 from .env_cfgs import astro_getup_env_cfg
 from .rl_cfg import astro_getup_ppo_runner_cfg
@@ -9,5 +10,5 @@ register_mjlab_task(
     env_cfg=astro_getup_env_cfg(),
     play_env_cfg=astro_getup_env_cfg(play=True),
     rl_cfg=astro_getup_ppo_runner_cfg(),
-    runner_cls=MjlabOnPolicyRunner,
+    runner_cls=GetupOnPolicyRunner,
 )
---
+++ src/mjlab_playground/tasks/getup/rl/__init__.py
@@ -0,0 +1,3 @@
+from mjlab_playground.tasks.getup.rl.runner import (
+    GetupOnPolicyRunner as GetupOnPolicyRunner,
+)
